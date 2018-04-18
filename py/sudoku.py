88ff23a2ad786e23d55168b8c21f041143191f20
Alejandro Socorro <alejandrosocorro@gmail.com>
2018-04-18 02:14:50 +0200

    First commit. Sudoku solver WIP.

---
+++ py/sudoku.py
@@ -0,0 +1,94 @@
+grid = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
+solved_puzzle = "483921657967345821251876493548132976729564138136798245372689514814253769695417382"
+
+rows = "ABCDEFGHI"
+cols = "123456789"
+
+def cross(a, b):
+    """
+    Return the list formed by all the possible concatenations
+    of a letter s in string a with a letter t in string b.
+    Args:
+        a, b: strings.
+    Returns:
+        list: All the possible concatenations of letters.
+    """
+    return [s+t for s in a for t in b]
+
+def grid_values(grid):
+    """
+    Convert grid string into {<box>: <value>} dict with '.' value for empties.
+    Args:
+        grid: Sudoku grid in string form, 81 characters long.
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'.
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+    """
+    assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
+
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+
+    assert len(values) == 81
+    return dict(zip(boxes, values))
+
+def eliminate(values):
+    """
+    Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit, '')
+    return values
+
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Args: 
+        The sudoku in dictionary form.
+    Output: None.
+    """
+    width = 1 + max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+# Element example:
+# row_units[0] = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
+# This is the top most row.
+
+column_units = [cross(rows, c) for c in cols]
+# Element example:
+# column_units[0] = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
+# This is the left most column.
+
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+# Element example:
+# square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+# This is the top left square.
+
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
