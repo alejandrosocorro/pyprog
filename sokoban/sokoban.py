e8538e12a1e92c9fc2bb3899a4a3eb932b1055eb
Alejandro Socorro <alejandrosocorro@gmail.com>
2019-07-30 21:51:38 +0200

    Python Sokoban Solver MVP

--- sokoban/sokoban.py
+++ sokoban/sokoban.py
@@ -1,215 +1,188 @@
-"""
-Empty = 0,
-Wall = 1,
-Whole = 2,
-Box = 3,
-Player = 4,
-BoxOnWhole = 5,
-PlayerOnWhole = 6
-"""
 import time
-import collections
-import ipdb
-import textwrap
+from collections import deque
 
 
-def transform_grid(grid):
-    new_grid = []
-    for row in grid:
-        line = row.replace("0", " ")
-        line = line.replace("1", "X")
-        line = line.replace("2", ".")
-        line = line.replace("3", "*")
-        line = line.replace("4", "@")
-        line = line.replace("5", "&")
-        line = line.replace("6", "$")
-        new_grid.append(line)
-    return new_grid
+class Reader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def transform_grid(self, grid):
+        new_grid = []
+        for row in grid:
+            line = row.replace("0", " ")
+            line = line.replace("1", "#")
+            line = line.replace("2", ".")
+            line = line.replace("3", "$")
+            line = line.replace("4", "@")
+            line = line.replace("5", "*")
+            line = line.replace("6", "+")
+            new_grid.append(line)
+        return new_grid
+
+    def read_sokobans(self):
+        with open(self.filename, "r") as f:
+            lines = f.readlines()
+
+        all_grids = []
+        for line in lines:
+            if line.strip():
+                line = line.split(",")[1].strip()
+                height = int(line[0:2])
+                width = int(line[2:4])
+                line = line[4:]
+                maze = [line[i : i + width] for i in range(0, len(line), width)]
+                all_grids.append(self.transform_grid(maze))
+        return all_grids
 
 
-def find_locations(grid):
-    box, hole, person, wall, box_on_whole, person_on_whole = (
-        "*",
-        ".",
-        "@",
-        "X",
-        "&",
-        "$",
-    )
-    boxes = []
-    holes = []
-    walls = []
-    player = ()
-    for i, row in enumerate(grid):
-        for j, column in enumerate(row):
-            if column == box:
-                boxes.append((i, j))
-            elif column == hole:
-                holes.append((i, j))
-            elif column == person:
-                player = (i, j)
-            elif column == wall:
-                walls.append((i, j))
-            elif column == box_on_whole:
-                boxes.append((i, j))
-            elif column == person_on_whole:
-                player = (i, j)
-    return boxes, holes, player, walls
+class Sokoban:
+    def __init__(self, board):
+        self.data = board
+        self.nrows = max(len(r) for r in self.data)
+        self.px = 0
+        self.py = 0
+        self.sdata = ""
+        self.ddata = ""
+        self.directions = (
+            (0, -1, "u", "U"),
+            (1, 0, "r", "R"),
+            (0, 1, "d", "D"),
+            (-1, 0, "l", "L"),
+        )
+        self.maps = {
+            " ": " ",
+            ".": ".",
+            "@": " ",
+            "#": "#",
+            "$": " ",
+            "*": ".",
+            "+": ".",
+        }
+        self.mapd = {
+            " ": " ",
+            ".": " ",
+            "@": "@",
+            "#": " ",
+            "$": "*",
+            "*": "*",
+            "+": "@",
+        }
+
+        for r, row in enumerate(self.data):
+            for c, ch in enumerate(row):
+                self.sdata += self.maps[ch]
+                self.ddata += self.mapd[ch]
+                if ch == "@":
+                    self.px, self.py = c, r
+
+    def solved_grid(self, m1, m2):
+        """ Print to stdout solved Sokoban board"""
+        m1 = [m1[i : i + self.nrows] for i in range(0, len(m1), self.nrows)]
+        m2 = [m2[i : i + self.nrows] for i in range(0, len(m2), self.nrows)]
+
+        rows = []
+        for i in range(0, len(m2)):
+            row = list(m2[i])
+            for j in range(0, len(m1[i]) - 1):
+                if m1[i][j] == "@":
+                    row[j] = "@"
+                elif m1[i][j] == "*":
+                    row[j] = "*"
+            rows.append("".join(row))
+        return rows
+
+    def push(self, x, y, dx, dy, data):
+        """ Possible push-moves for box and update player """
+        if (
+            self.sdata[(y + 2 * dy) * self.nrows + x + 2 * dx] == "#"
+            or data[(y + 2 * dy) * self.nrows + x + 2 * dx] != " "
+        ):
+            return None
+
+        row = list(data)
+        row[y * self.nrows + x] = " "
+        row[(y + dy) * self.nrows + x + dx] = "@"
+        row[(y + 2 * dy) * self.nrows + x + 2 * dx] = "*"
+        return "".join(row)
+
+    def is_solved(self, temp):
+        """ Helper function to check if the Sokoban is solved """
+        for i in range(len(temp)):
+            if (self.sdata[i] == ".") != (temp[i] == "*"):
+                return False
+        return True
+
+    def solve(self):
+        """ Search solution """
+        grid = None
+        queue = deque([(self.ddata, "", self.px, self.py)])
+        visited = set([self.ddata])
+        while queue:
+            current, csol, x, y = queue.popleft()
+
+            for di in self.directions:
+                temp = current
+                dx, dy = di[0], di[1]
+                if temp[(y + dy) * self.nrows + x + dx] == "*":
+                    temp = self.push(x, y, dx, dy, temp)
+                    if temp and temp not in visited:
+
+                        if self.is_solved(temp):
+                            grid = self.solved_grid(temp, self.sdata)
+                            return grid, csol + di[3]
+
+                        queue.append((temp, csol + di[3], x + dx, y + dy))
+                        visited.add(temp)
+                else:
+
+                    if (
+                        self.sdata[(y + dy) * self.nrows + x + dx] == "#"
+                        or temp[(y + dy) * self.nrows + x + dx] != " "
+                    ):
+                        continue
+
+                    row = list(temp)
+                    row[y * self.nrows + x] = " "
+                    row[(y + dy) * self.nrows + x + dx] = "@"
+                    temp = "".join(row)
+
+                    if temp not in visited:
+
+                        if self.is_solved(temp):
+                            grid = self.solved_grid(temp, self.sdata)
+                            return grid, csol + di[2]
+
+                        queue.append((temp, csol + di[2], x + dx, y + dy))
+                        visited.add(temp)
+
+        return grid, None
 
 
-def push_box(start, end, grid):
-    """
-    * Check if the player can move the box
-    * Return True if possible and update the grid
-    * Return False if not and return grid
-    """
-    x, y = start
-    neighbors = find_neighbors(x, y)
+def solve_all(grids):
+    """ Try to solve a sequence of sokoban grids """
+    sokobans = [Sokoban(grid) for grid in grids]
+    for i, sokoban in enumerate(sokobans):
+        time, result = time_solve(sokoban)
+        print(f"Solving Sokoban #{i}\n")
+        if result:
+            for row in result[0]:
+                print(row)
+
+            print()
+            print(f"# of Steps: {len(result[1])}")
+            print(f"Time: {time}\n\n")
+        else:
+            print("Couldn't solve Sokoban #{i}...")
 
 
-def choose_box_to_push(grid):
-    boxes, holes, player, walls = find_locations(grid)
-    paths = []
-    for box in boxes:
-        paths.append(bfs(grid, box, player))
-    return min(paths)
-
-
-def find_nearest_hole(start, grid):
-    _, holes, _, _ = find_locations(grid)
-    paths = []
-    for hole in holes:
-        paths.append(bfs(grid, start, hole))
-    return min(paths)
-
-
-def can_push(cell):
-    pass
-
-
-def can_move(cell):
-    x, y = cell
-    return True if grid[x][y] in (" ", ".") else False
-
-
-def get_grid_X_and_Y(grid):
-    width = max([len(r) for r in grid])
-    height = len(grid)
-    return width, height
-
-
-def move_player_trough_route(player, route, grid):
-    x, y = route[0]
-    neighbors = find_neighbors(x, y)
-    pos = [i for i in neighbors if i in route][0]
-    temp = list(grid[pos[0]])
-    temp[pos[1]] = "@"
-    grid[pos[0]] = "".join(temp)
-
-    temp = list(grid[player[0]])
-    replace_char = " "
-    if player[1] == "$":
-        replace_char = "."
-    temp[player[1]] = replace_char
-    grid[player[0]] = "".join(temp)
-    return grid
-
-
-def find_neighbors(x, y):
-    return ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
-
-
-def bfs(grid, start, goal):
-    width, height = get_grid_X_and_Y(grid)
-    wall = "X"
-    box = "*"
-    box_on_whole = "&"
-    queue = collections.deque([[start]])
-    seen = set([start])
-    while queue:
-        path = queue.popleft()
-        x, y = path[-1]
-        if (x, y) == goal:
-            return path
-
-        for row, col in find_neighbors(x, y):
-            if (
-                0 <= col < width
-                and 0 <= row < height
-                and grid[row][col] != wall
-                and grid[row][col] != box
-                and grid[row][col] != box_on_whole
-                and (row, col) not in seen
-            ):
-                queue.append(path + [(row, col)])
-                seen.add((row, col))
-
-
-def read_sokobans(filename):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-
-    all_grids = []
-    for line in lines:
-        if line.strip():
-            line = line.split(",")[1].strip()
-            height = int(line[0:2])
-            width = int(line[2:4])
-            maze = textwrap.wrap(line[4:], width)
-            all_grids.append(transform_grid(maze))
-    return all_grids
-
-
-def display_sokoban(grid):
-    for row in grid:
-        print(row)
-
-
-def search(grid):
-    # First, reduce the puzzle using the previous function
-    values = reduce_puzzle(grid)
-    if values is False:
-        return False  ## Failed earlier
-
-    if all(len(values[s]) == 1 for s in boxes):
-        return values  ## Solved!
-
-    # Look for unfilled squares and choose one with the fewest possibilities
-    _, box = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
-
-    # Recursive search to solve each one of the resulting sudokus
-    for val in values[box]:
-        sudoku_copy = values.copy()
-        sudoku_copy[box] = val
-        attempt = search(sudoku_copy)
-        if attempt:
-            return attempt
-
-
-def solve(grid):
-    return search(grid)
-
-
-def solve_all(grids, name=""):
-    """Attempt to solve a sequence of grids. Report results."""
-    times, results = zip(*[time_solve(grid) for grid in grids])
-    if len(results):
-        print(results[0])
-
-
-def time_solve(grid):
-    start = time.time()
-    result = solve(grid)
-    t = time.time() - start
-    return (t, solved(result), result)
-
-
-def solved(grid):
-    for _, row in grid.items():
-        if "." in row:
-            return False
-    return True
+def time_solve(sokoban):
+    """ Time how long it takes to solve a Sokoban game """
+    t0 = time.time()
+    result = sokoban.solve()
+    return (time.time() - t0, result)
 
 
 if __name__ == "__main__":
-    solve_all(parse_sokobans("sokoban_evels.txt"))
+    reader = Reader("levels.txt")
+    all_grids = reader.read_sokobans()
+    solve_all(all_grids)
